abe26be95e7dae1f49624ff0894e4d8737cb560b
sturm30 <informatico.del.borgo@gmail.com>
2021-01-27 12:29:37 +0100

    Sabbie Mobili

--- doc.gd
+++ doc.gd
@@ -2,10 +2,15 @@ extends KinematicBody2D
 
 export (int) var run_speed = 100
 export (int) var jump_speed = -400
+export (int) var jump_speed_default = -400
 export (int) var gravity = 1200
+export (int) var gravity_default = 1200
 
 var velocity = Vector2()
 var jumping = false
+
+enum STATES {WALKING, JUMPING, SABBIEMOBILI, RAFFICA}
+onready var state = STATES.WALKING
 
 func get_input():
 	velocity.x = 0
@@ -16,6 +21,10 @@ func get_input():
 	if jump and is_on_floor():
 		jumping = true
 		velocity.y = jump_speed
+	if jump and state == STATES.SABBIEMOBILI:
+		velocity.y = jump_speed
+	if state == STATES.RAFFICA:
+		velocity.x = -20
 	if right:
 		velocity.x += run_speed
 	if left:
@@ -27,3 +36,17 @@ func _physics_process(delta):
 	if jumping and is_on_floor():
 		jumping = false
 	velocity = move_and_slide(velocity, Vector2(0, -1))
+
+
+func _on_Area2D_body_entered(body):
+	state = STATES.SABBIEMOBILI
+	gravity = 2000
+	jump_speed = -300
+	pass # Replace with function body.
+
+
+func _on_Area2D_body_exited(body):
+	state = STATES.WALKING
+	gravity = gravity_default
+	jump_speed = jump_speed_default
+	pass # Replace with function body.
