bd0d71663bac6c41073dc04d54d8f1964fb3eb99
sturm30 <informatico.del.borgo@gmail.com>
2021-01-28 10:46:59 +0100

    player in tempesta

--- doc.gd
+++ doc.gd
@@ -7,6 +7,7 @@ export (int) var gravity = 1200
 export (int) var gravity_default = 1200
 
 var velocity = Vector2()
+var relative_velocity = Vector2()
 var jumping = false
 var energy = 100
 
@@ -16,11 +17,13 @@ onready var state = STATES.WALKING
 signal camera_shake_requested
 signal camera_shake_stop
 
+var left
+var right
 
 func get_input():
-	velocity.x = 0
-	var right = Input.is_action_pressed('ui_right')
-	var left = Input.is_action_pressed('ui_left')
+	velocity.x = relative_velocity.x
+	right = Input.is_action_pressed('ui_right')
+	left = Input.is_action_pressed('ui_left')
 	var jump = Input.is_action_just_pressed('ui_select')
 
 	if jump and is_on_floor():
@@ -28,8 +31,6 @@ func get_input():
 		velocity.y = jump_speed
 	if jump and state == STATES.SABBIEMOBILI:
 		velocity.y = jump_speed
-	if state == STATES.RAFFICA:
-		velocity.x = -20
 	if right:
 		velocity.x += run_speed
 	if left:
@@ -44,11 +45,18 @@ func _physics_process(delta):
 	
 	velocity.y += gravity * delta
 	
-	if velocity.x != 0 :
+	#if velocity.x != 0 and (
+	if right or left:
 		$AnimatedSprite.play("Run")
-		$AnimatedSprite.flip_h = velocity.x < 0	
+	if right:
+		$AnimatedSprite.flip_h = false
+	if left:
+		$AnimatedSprite.flip_h =true
+#		if relative_velocity == Vector2.ZERO or left:
+#			$AnimatedSprite.flip_h = velocity.x < 0
 		
-	if velocity.x == 0 :
+	#if velocity.x == 0 :
+	if !left and !right:
 		$AnimatedSprite.play("Idle")
 	
 	if jumping and is_on_floor():
@@ -98,4 +106,16 @@ func _on_Area2D_area_shape_entered(area_id, area, area_shape, self_shape):
 
 func _on_timerGameOver_timeout():
 	get_tree().change_scene("res://GameOver.tscn")
+	pass # Replace with function body.
+
+
+func _on_Area2D_area_entered(area):
+	if area.is_in_group('a_tempesta'):
+		relative_velocity = Vector2(-150, 0)
+	pass # Replace with function body.
+
+
+func _on_Area2D_area_exited(area):
+	if area.is_in_group('a_tempesta'):
+		relative_velocity = Vector2.ZERO
 	pass # Replace with function body.
